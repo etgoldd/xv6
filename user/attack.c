4e62af102ec1b33ef6fd01fc117bceb8fd7b1ae3
Ethan <etgold05@gmail.com>
2025-03-31 19:44:59 +0100

    done

--- user/attack.c
+++ user/attack.c
@@ -3,11 +3,11 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
+#define sec_start "my very "
+int main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
-  exit(1);
+  char *end = sbrk(PGSIZE * 32);
+  end += 16 * PGSIZE;
+  write(2, end + 32, 8);
+  exit(0);
 }
--- user/secret.c
+++ user/secret.c
@@ -3,18 +3,17 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-  if(argc != 2){
+  if (argc != 2)
+  {
     printf("Usage: secret the-secret\n");
     exit(1);
   }
-  char *end = sbrk(PGSIZE*32);
+  char *end = sbrk(PGSIZE * 32);
   end = end + 9 * PGSIZE;
   strcpy(end, "my very very very secret pw is:   ");
-  strcpy(end+32, argv[1]);
+  strcpy(end + 32, argv[1]);
+  // printf("what %d\n", *(end + 32));
   exit(0);
 }
-
